e5cec2c522537ab2191899ae3c4efa036c2fb71d
Andrew Byerly <andrew.Byerly@idcomlog.com>
2018-11-21 11:56:39 -0600

    created FPPResponse class

---
+++ FPPResponse.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Light_Emoting_Diode
+{
+    class FPPResponse
+    {
+        public string image_id { get; set; }
+        public string request_id { get; set; }
+        public int time_used { get; set; }
+        public Face[] faces { get; set; }
+    }
+
+    public class Face
+    {
+        public Attributes attributes { get; set; }
+        public Face_Rectangle face_rectangle { get; set; }
+        public string face_token { get; set; }
+    }
+
+    public class Attributes
+    {
+        public Emotion emotion { get; set; }
+    }
+
+    public class Emotion
+    {
+        public int sadness { get; set; }
+        public int neutral { get; set; }
+        public int disgust { get; set; }
+        public int anger { get; set; }
+        public int surprise { get; set; }
+        public int fear { get; set; }
+        public int happiness { get; set; }
+    }
+
+    public class Face_Rectangle
+    {
+        public int width { get; set; }
+        public int top { get; set; }
+        public int left { get; set; }
+        public int height { get; set; }
+    }
+}
